11708f967f012bb2e9b648ebb78a1f71e95b55ac
Anuj940 <anujvishwakarma9827@gmail.com>
2025-12-16 17:10:25 +0530

    [Anuj] : added CalculateSimpleInterest

--- gcr-codebase/Program-Requisite/CalculateSimpleInterest.java
+++ gcr-codebase/Program-Requisite/CalculateSimpleInterest.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class CalculateSimpleInterest{
+public class CalculateSimpleInterest {
     public static void main (String[]args){
         Scanner Sc=new Scanner(System.in);
 		int Principal=Sc.nextInt();
