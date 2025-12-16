54e4d6fb4d5aed04da1e88efc43da51e57661bbd
Anuj940 <anujvishwakarma9827@gmail.com>
2025-12-16 17:26:15 +0530

    [Anuj] : added PowerCalculation

---
+++ gcr-codebase/Program-Requisite/PowerCalculation.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class PowerCalculation{
+    public static void main (String[]args){
+        Scanner Sc=new Scanner(System.in);
+		int base=Sc.nextInt();
+		int exponential=Sc.nextInt();
+		System.out.println(Math.pow(base,exponential));
+    }
+}
