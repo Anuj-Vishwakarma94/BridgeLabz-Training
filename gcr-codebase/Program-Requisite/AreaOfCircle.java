0974e2f03fbde378072288eb840e4f302fb3a81b
Anuj940 <anujvishwakarma9827@gmail.com>
2025-12-16 16:42:54 +0530

    [Anuj] : added initial setup

---
+++ gcr-codebase/Program-Requisite/AreaOfCircle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class AreaOfCircle{
+    public static void main (String[]args){
+        Scanner Sc=new Scanner(System.in);
+		int Radius=Sc.nextInt();
+		int Area=(22/7)*Radius*Radius;
+		System.out.println(Area);
+    }
+}
