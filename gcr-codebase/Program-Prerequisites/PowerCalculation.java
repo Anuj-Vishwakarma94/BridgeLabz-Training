885a66b35e71353cadd4e125272dd6913f16fa77
Anuj940 <anujvishwakarma9827@gmail.com>
2025-12-17 13:31:40 +0530

    [Anuj] : added PowerCalculation

--- gcr-codebase/Program-Prerequisites/PowerCalculation.java
+++ gcr-codebase/Program-Prerequisites/PowerCalculation.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class PowerCalculation{
+public class PowerCalculation {
     public static void main (String[]args){
         Scanner Sc=new Scanner(System.in);
 		int base=Sc.nextInt();
