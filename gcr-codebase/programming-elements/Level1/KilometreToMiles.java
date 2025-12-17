cfdd82a0d1809d0560c82955d0ed0500b08640d8
Anuj940 <anujvishwakarma9827@gmail.com>
2025-12-18 00:44:18 +0530

    [Anuj] : added KilometreToMiles

---
+++ gcr-codebase/programming-elements/Level1/KilometreToMiles.java
@@ -0,0 +1,12 @@
+//Created class Named KilometreToMiles
+public class KilometreToMiles{
+    public static void main(String args[]){
+
+    //take kilometres as the input and then convert it to miles
+	double kilometres = 10.8;
+	double miles  = (10.8/1.6);
+	
+	//print the distance in kilometres as well as miles 
+	System.out.println("The distance " + kilometres + " km in miles is " + miles); 
+	}
+}
