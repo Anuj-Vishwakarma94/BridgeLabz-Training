07ad8c1b18e48e0c65a2c7963c2355d96d715c0a
Anuj940 <anujvishwakarma9827@gmail.com>
2025-12-23 11:08:15 +0530

    [Anuj] : added WordLengthTable

---
+++ gcr-codebase/strings/Level-2/WordLengthTable.java
@@ -0,0 +1,93 @@
+//Created class Named WordLengthTable
+import java.util.Scanner;
+public class WordLengthTable{
+   public static void main(String[]args){
+
+    Scanner input=new Scanner(System.in);
+
+    //take complete text input
+    String text=input.nextLine();
+
+    //split text into words using user-defined method
+    String words[]=splitTextUsingCharAt(text);
+
+    //create 2D array of words and their lengths
+    String wordLengthTable[][]=createWordLengthTable(words);
+
+    //display the result in tabular format
+    System.out.println("Word\tLength");
+
+    for(int i=0;i<wordLengthTable.length;i++){
+        int length=Integer.parseInt(wordLengthTable[i][1]);
+        System.out.println(wordLengthTable[i][0] + "\t" + length);
+    }
+   }
+
+   //method to find string length without using length()
+   public static int findLengthWithoutUsingLength(String text){
+       int count=0;
+
+       try{
+           while(true){
+               text.charAt(count);
+               count++;
+           }
+       }
+       catch(StringIndexOutOfBoundsException e){
+       }
+
+       return count;
+   }
+
+   //method to split text into words using charAt()
+   public static String[] splitTextUsingCharAt(String text){
+       int length=findLengthWithoutUsingLength(text);
+       int wordCount=1;
+
+       //count words
+       for(int i=0;i<length;i++){
+           if(text.charAt(i)==' '){
+               wordCount++;
+           }
+       }
+
+       int spaceIndex[]=new int[wordCount+1];
+       spaceIndex[0]=-1;
+       int index=1;
+
+       //store space indexes
+       for(int i=0;i<length;i++){
+           if(text.charAt(i)==' '){
+               spaceIndex[index]=i;
+               index++;
+           }
+       }
+       spaceIndex[index]=length;
+
+       String words[]=new String[wordCount];
+
+       //extract words
+       for(int i=0;i<wordCount;i++){
+           String word="";
+           for(int j=spaceIndex[i]+1;j<spaceIndex[i+1];j++){
+               word=word+text.charAt(j);
+           }
+           words[i]=word;
+       }
+
+       return words;
+   }
+
+   //method to create 2D array of word and its length
+   public static String[][] createWordLengthTable(String words[]){
+       String table[][]=new String[words.length][2];
+
+       for(int i=0;i<words.length;i++){
+           int len=findLengthWithoutUsingLength(words[i]);
+           table[i][0]=words[i];
+           table[i][1]=String.valueOf(len);
+       }
+
+       return table;
+   }
+}
