6fba58be8db25a71d6bc8a81b95cbb1aa463f9ad
Anuj940 <anujvishwakarma9827@gmail.com>
2025-12-28 00:15:09 +0530

    [Anuj] : added Employee

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/level1/Employee.java
@@ -0,0 +1,32 @@
+package com.constructors.level1;
+
+public class Employee {
+	
+	public int employeeID;
+    protected String department;
+    private double salary;
+
+    public Employee(int employeeID, String department, double salary) {
+        this.employeeID = employeeID;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        if (salary >= 0) {
+            this.salary = salary;
+        } else {
+            System.out.println("Invalid salary value!");
+        }
+    }
+
+    public void displayEmployeeDetails() {
+        System.out.println("Employee ID: " + employeeID);
+        System.out.println("Department: " + department);
+        System.out.println("Salary: " + salary);
+    }
+}
