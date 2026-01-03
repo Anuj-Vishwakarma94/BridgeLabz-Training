520bd601fe268494cedfe5cd499ee0ae9c94f831
Anuj940 <anujvishwakarma9827@gmail.com>
2026-01-03 10:43:29 +0530

    [Anuj] : added Transaction

---
+++ java-oops-practice/scenario-based-codebase/oops_scenario_based/src/com/ewallet_application/Transaction.java
@@ -0,0 +1,19 @@
+package com.ewallet_application;
+import java.time.LocalDateTime;
+
+public class Transaction {
+
+    private String details;
+    private double amount;
+    private LocalDateTime time;
+
+    public Transaction(String details, double amount) {
+        this.details = details;
+        this.amount = amount;
+        this.time = LocalDateTime.now();
+    }
+
+    public void printTransaction() {
+        System.out.println(time + " | " + details + " | ₹" + amount);
+    }
+}
