3a50ac4fdf9da4c8a1ca47d20c9b711e5678e437
Anuj940 <anujvishwakarma9827@gmail.com>
2026-01-03 11:00:45 +0530

    [Anuj] : added Undergraduate

---
+++ java-oops-practice/scenario-based-codebase/oops_scenario_based/src/com/university_course_management_system/Undergraduate.java
@@ -0,0 +1,16 @@
+package com.university_course_management_system;
+public class Undergraduate extends Student {
+
+    public Undergraduate(String id, String name) {
+        super(id, name);
+    }
+
+    public Undergraduate(String id, String name, String elective) {
+        super(id, name, elective);
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Undergraduate Student => " + getName());
+    }
+}
