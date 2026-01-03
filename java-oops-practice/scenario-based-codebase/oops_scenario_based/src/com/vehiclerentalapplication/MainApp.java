24f5ef6a6bbeba38cfd088009778202bc30b631c
Anuj940 <anujvishwakarma9827@gmail.com>
2026-01-03 12:11:13 +0530

    [Anuj] : added MainApp

---
+++ java-oops-practice/scenario-based-codebase/oops_scenario_based/src/com/vehiclerentalapplication/MainApp.java
@@ -0,0 +1,27 @@
+package com.vehiclerentalapplication;
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        Customer c1 = new Customer("C101", "Anuj Vishwakarma");
+        c1.displayInfo();
+
+        System.out.println();
+
+        Vehicle v1 = new Bike("B001", "Yamaha Fz-s", 300, true);
+        Vehicle v2 = new Car("C201", "Koenigsegg Jesko", 2000, true);
+        Vehicle v3 = new Truck("T501", "Tata Motors", 3500, 8);
+
+        Vehicle[] vehicles = {v1, v2, v3};
+
+        for (Vehicle v : vehicles) {
+            v.displayInfo();
+        }
+
+        System.out.println("\n--- RENT DETAILS ---");
+
+        System.out.println("Bike Rent (6 days): ₹" + v1.calculateRent(6));
+        System.out.println("Car Rent (8 days): ₹" + v2.calculateRent(8));
+        System.out.println("Truck Rent (3 days): ₹" + v3.calculateRent(3));
+    }
+}
