d0bb4735b57fa64fa68153aed1b7c2db8c582724
Anuj940 <anujvishwakarma9827@gmail.com>
2026-01-03 11:43:40 +0530

    [Anuj] : added BankSystem

---
+++ java-oops-practice/scenario-based-codebase/oops_scenario_based/src/com/bankmanagementsystem/BankSystem.java
@@ -0,0 +1,35 @@
+package com.bankmanagementsystem;
+import java.util.*;
+public class BankSystem {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+
+        // Create Customer
+        Customer c1 = new Customer("Rahul Verma", "CUS101");
+        c1.displayCustomer();
+
+        // Create Account
+        SavingsAccount sa = new SavingsAccount("SAV123", 12000, 4.5);
+        Validator.printAccountSummary(sa);
+
+        LoanManager loanManager = new LoanManager();
+
+        // Validate + Loan Check
+        if (loanManager.checkLoanEligibility(sa)) {
+            System.out.println("🎉 Customer is eligible for a loan.");
+
+            double eligibleAmount = loanManager.calculateEligibleAmount(sa);
+            System.out.println("Eligible Loan Amount: ₹" + eligibleAmount);
+
+            double discount = loanManager.calculateDiscount(sa, eligibleAmount);
+            System.out.println("Discount Applicable: ₹" + discount);
+
+            System.out.println("Final Loan Processing Amount: ₹" + (eligibleAmount - discount));
+
+        } else {
+            System.out.println("❌ Customer is NOT eligible for a loan.");
+        }
+
+        sc.close();
+    }
+}
