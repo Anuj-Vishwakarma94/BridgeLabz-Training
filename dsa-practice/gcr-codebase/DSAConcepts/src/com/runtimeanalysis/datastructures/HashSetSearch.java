f317831825e2f5617aabc5d3564dd7969439fef4
Anuj940 <anujvishwakarma9827@gmail.com>
2026-01-09 13:20:42 +0530

    [Anuj] : added HashSetSearch

---
+++ dsa-practice/gcr-codebase/DSAConcepts/src/com/runtimeanalysis/datastructures/HashSetSearch.java
@@ -0,0 +1,10 @@
+package com.runtimeanalysis.datastructures;
+
+import java.util.Set;
+
+public class HashSetSearch {
+
+	public static boolean search(Set<Integer> set, int target) {
+		return set.contains(target);
+	}
+}
