36ac431dc8b87178e7f57d2531d8a56eb4c65e72
Anuj940 <anujvishwakarma9827@gmail.com>
2026-01-12 13:12:10 +0530

    [Anuj] : added Customer

---
+++ dsa-practice/scenario-based-codebase/dsa_scenario_based/src/com/callcentre/Customer.java
@@ -0,0 +1,12 @@
+package com.callcentre;
+
+public class Customer {
+	String name;
+	boolean isVip;
+	int callTime;
+	Customer(String name, boolean isVip, int callTime){
+		this.name=name;
+		this.isVip=isVip;
+		this.callTime=callTime;
+	}
+}
