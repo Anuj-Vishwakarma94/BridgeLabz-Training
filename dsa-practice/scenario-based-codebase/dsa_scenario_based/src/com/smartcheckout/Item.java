4dc6b8f251ffbfe4434df2b20fb215b022472bc0
Anuj940 <anujvishwakarma9827@gmail.com>
2026-01-13 17:09:40 +0530

    [Anuj] : added Item

---
+++ dsa-practice/scenario-based-codebase/dsa_scenario_based/src/com/smartcheckout/Item.java
@@ -0,0 +1,13 @@
+package com.smartcheckout;
+
+public class Item {
+    String name;
+    int price;
+    int stock;
+
+    public Item(String name, int price, int stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+}
