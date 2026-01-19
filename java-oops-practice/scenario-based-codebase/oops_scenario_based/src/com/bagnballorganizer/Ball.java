0513e949b7863f35cd19f3628f95e1fc5a60150e
Anuj940 <anujvishwakarma9827@gmail.com>
2026-01-19 12:20:10 +0530

    [Anuj] : added Ball

---
+++ java-oops-practice/scenario-based-codebase/oops_scenario_based/src/com/bagnballorganizer/Ball.java
@@ -0,0 +1,25 @@
+package com.bagnballorganizer;
+public class Ball {
+
+    private String id;
+    private String color;
+    private String size; // small / medium / large
+
+    public Ball(String id, String color, String size) {
+        this.id = id;
+        this.color = color;
+        this.size = size;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+}
