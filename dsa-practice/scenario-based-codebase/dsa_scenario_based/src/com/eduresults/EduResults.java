db0f21ad49efc7f9f37376962d5d588256c4db15
Anuj940 <anujvishwakarma9827@gmail.com>
2026-01-19 15:20:49 +0530

    [Anuj] : added EduResults

---
+++ dsa-practice/scenario-based-codebase/dsa_scenario_based/src/com/eduresults/EduResults.java
@@ -0,0 +1,39 @@
+package com.eduresults;
+import java.util.Scanner;
+
+public class EduResults {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" EduResults – State Rank Generator ");
+        System.out.println("------------------------------------\n");
+
+        System.out.print("Enter total number of students: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        Student[] students = new Student[n];
+
+        System.out.println("\nEnter student details :");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Student name: ");
+            String name = sc.nextLine();
+
+            System.out.print("Score: ");
+            int score = sc.nextInt();
+            sc.nextLine();
+
+            students[i] = new Student(name, score);
+        }
+
+        System.out.println("\nBefore Merging:");
+        RankMerger.display(students);
+
+        RankMerger.mergeSort(students, 0, n - 1);
+
+        System.out.println("\nFinal State-Level Rank List:");
+        RankMerger.display(students);
+
+        sc.close();
+    }
+}
