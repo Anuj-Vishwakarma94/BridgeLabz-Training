6bed63282893592997098576b1f418f7d06cd65d
Anuj940 <anujvishwakarma9827@gmail.com>
2026-01-21 14:48:04 +0530

    [Anuj] : added Medicine

---
+++ dsa-practice/scenario-based-codebase/dsa_scenario_based/src/com/medwarehouse/Medicine.java
@@ -0,0 +1,10 @@
+package com.medwarehouse;
+public class Medicine {
+    String name;
+    int expiryDays;
+
+    public Medicine(String name, int expiryDays) {
+        this.name = name;
+        this.expiryDays = expiryDays;
+    }
+}
