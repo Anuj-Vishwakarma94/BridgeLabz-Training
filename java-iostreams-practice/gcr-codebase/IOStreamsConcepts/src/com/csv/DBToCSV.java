eeb0bd697a4c5a824432ef25775eb70d6fa72dfc
Anuj940 <anujvishwakarma9827@gmail.com>
2026-01-26 22:36:20 +0530

    [Anuj] : added DBToCSV

---
+++ java-iostreams-practice/gcr-codebase/IOStreamsConcepts/src/com/csv/DBToCSV.java
@@ -0,0 +1,41 @@
+package com.csv;
+
+import java.io.*;
+import java.sql.*;
+
+public class DBToCSV {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/test?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true",
+                "root",
+                "Gamebit@8878"
+            );
+
+            Statement s = c.createStatement();
+            ResultSet rs = s.executeQuery("select * from employee");
+
+            BufferedWriter bw = new BufferedWriter(new FileWriter("employees.csv"));
+            bw.write("ID,Name,Department,Salary\n");
+
+            while (rs.next()) {
+                bw.write(
+                    rs.getInt("id") + "," +
+                    rs.getString("name") + "," +
+                    rs.getString("department") + "," +
+                    rs.getInt("salary") + "\n"
+                );
+            }
+
+            bw.close();
+            c.close();
+
+            System.out.println("DB → CSV successful");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
