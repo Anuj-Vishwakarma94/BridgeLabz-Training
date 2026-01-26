6a096c8bd903dec27cbc529731fa1795c6484899
Anuj940 <anujvishwakarma9827@gmail.com>
2026-01-26 22:38:03 +0530

    [Anuj] : added EncryptCSV

---
+++ java-iostreams-practice/gcr-codebase/IOStreamsConcepts/src/com/csv/EncryptCSV.java
@@ -0,0 +1,14 @@
+package com.csv;
+
+import java.io.*;
+import java.util.Base64;
+
+public class EncryptCSV {
+    public static void main(String[] args) throws Exception {
+        String data = "anuj@gmail.com";
+        String enc = Base64.getEncoder().encodeToString(data.getBytes());
+        String dec = new String(Base64.getDecoder().decode(enc));
+        System.out.println(enc);
+        System.out.println(dec);
+    }
+}
