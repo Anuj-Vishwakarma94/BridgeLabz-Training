fb546b6830827048f3e051a738f6bdf513dbad15
Anuj940 <anujvishwakarma9827@gmail.com>
2026-01-27 14:47:06 +0530

    [Anuj] : added FilterStudents

--- java-iostreams-practice/gcr-codebase/IOStreamsConcepts/src/com/csv/FilterStudents.java
+++ java-iostreams-practice/gcr-codebase/IOStreamsConcepts/src/com/csv/FilterStudents.java
@@ -4,11 +4,11 @@ import java.io.*;
 
 public class FilterStudents {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new FileReader("students.csv"));
+        BufferedReader br = new BufferedReader(new FileReader("src/com/csv/students.csv"));
         String line = br.readLine();
         while ((line = br.readLine()) != null) {
             String[] d = line.split(",");
-            if (Integer.parseInt(d[3]) > 80)
+            if (Integer.parseInt(d[2]) > 20)
                 System.out.println(line);
         }
         br.close();
