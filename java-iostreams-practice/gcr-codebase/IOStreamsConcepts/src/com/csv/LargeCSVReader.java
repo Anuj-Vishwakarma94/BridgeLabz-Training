f7198963c3ec950fe303ae113f87a616ed3aeeae
Anuj940 <anujvishwakarma9827@gmail.com>
2026-01-27 14:49:03 +0530

    [Anuj] : added LargeCSVReader

--- java-iostreams-practice/gcr-codebase/IOStreamsConcepts/src/com/csv/LargeCSVReader.java
+++ java-iostreams-practice/gcr-codebase/IOStreamsConcepts/src/com/csv/LargeCSVReader.java
@@ -4,13 +4,21 @@ import java.io.*;
 
 public class LargeCSVReader {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new FileReader("large.csv"));
+
+        BufferedReader br = new BufferedReader(
+                new FileReader("src/com/csv/large.csv"));
+
+        String line;
         int count = 0;
-        while (br.readLine() != null) {
+
+        while ((line = br.readLine()) != null) {
             count++;
-            if (count % 100 == 0)
+            if (count % 100 == 0) {
                 System.out.println("Processed: " + count);
+            }
         }
+
         br.close();
+        System.out.println("Total lines: " + count);
     }
 }
