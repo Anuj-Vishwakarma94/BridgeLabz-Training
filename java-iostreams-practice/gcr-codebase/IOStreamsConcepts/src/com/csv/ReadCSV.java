7b8281dc39063076b97025cd4db5d565b3ca8dee
Anuj940 <anujvishwakarma9827@gmail.com>
2026-01-27 14:51:12 +0530

    [Anuj] : added ReadCSV

--- java-iostreams-practice/gcr-codebase/IOStreamsConcepts/src/com/csv/ReadCSV.java
+++ java-iostreams-practice/gcr-codebase/IOStreamsConcepts/src/com/csv/ReadCSV.java
@@ -4,13 +4,26 @@ import java.io.*;
 
 public class ReadCSV {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new FileReader("students.csv"));
+
+        BufferedReader br = new BufferedReader(
+                new FileReader("src/com/csv/students.csv"));
+
         String line;
-        br.readLine();
+
+        br.readLine(); // skip header
+
         while ((line = br.readLine()) != null) {
-            String[] d = line.split(",");
-            System.out.println("ID=" + d[0] + " Name=" + d[1] + " Age=" + d[2] + " Marks=" + d[3]);
+            String[] data = line.split(",");
+
+            if (data.length < 3) continue;
+
+            String id = data[0];
+            String name = data[1];
+            String age = data[2];
+
+            System.out.println(id + " " + name + " " + age);
         }
+
         br.close();
     }
 }
