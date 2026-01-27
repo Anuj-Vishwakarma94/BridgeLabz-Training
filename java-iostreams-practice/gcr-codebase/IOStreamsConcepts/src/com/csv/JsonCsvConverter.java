37fd0518d84485158b7285d73a656267ac4a6585
Anuj940 <anujvishwakarma9827@gmail.com>
2026-01-27 14:48:21 +0530

    [Anuj] : added JsonCsvConverter

--- java-iostreams-practice/gcr-codebase/IOStreamsConcepts/src/com/csv/JsonCsvConverter.java
+++ java-iostreams-practice/gcr-codebase/IOStreamsConcepts/src/com/csv/JsonCsvConverter.java
@@ -4,15 +4,33 @@ import java.io.*;
 
 public class JsonCsvConverter {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new FileReader("students.json"));
-        BufferedWriter bw = new BufferedWriter(new FileWriter("students.csv"));
-        String line;
+
+        BufferedReader br = new BufferedReader(
+                new FileReader("src/com/csv/students.json"));
+        BufferedWriter bw = new BufferedWriter(
+                new FileWriter("src/com/csv/students.csv"));
+
         bw.write("id,name,age\n");
+
+        String id = "", name = "", age = "";
+        String line;
+
         while ((line = br.readLine()) != null) {
-            if (line.contains("id"))
-                bw.write(line.replaceAll("[^0-9,A-Za-z]", "") + "\n");
+            if (line.contains("\"id\""))
+                id = line.replaceAll("[^0-9]", "");
+            else if (line.contains("\"name\""))
+                name = line.replaceAll("[^A-Za-z]", "");
+            else if (line.contains("\"age\""))
+                age = line.replaceAll("[^0-9]", "");
+
+            if (!id.isEmpty() && !name.isEmpty() && !age.isEmpty()) {
+                bw.write(id + "," + name + "," + age + "\n");
+                id = name = age = "";
+            }
         }
+
         br.close();
         bw.close();
+        System.out.println("CSV file created successfully");
     }
 }
