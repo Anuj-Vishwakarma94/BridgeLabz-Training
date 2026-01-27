bc2f84c3d9dbf7ae858342b09157b7024f61bded
Anuj940 <anujvishwakarma9827@gmail.com>
2026-01-27 14:44:34 +0530

    [Anuj] : added CountRows

--- java-iostreams-practice/gcr-codebase/IOStreamsConcepts/src/com/csv/CountRows.java
+++ java-iostreams-practice/gcr-codebase/IOStreamsConcepts/src/com/csv/CountRows.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class CountRows {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new FileReader("students.csv"));
+        BufferedReader br = new BufferedReader(new FileReader("src/com/csv/students.csv"));
         int count = -1;
         while (br.readLine() != null) count++;
         br.close();
